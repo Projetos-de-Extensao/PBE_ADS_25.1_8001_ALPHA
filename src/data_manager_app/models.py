545fc3aa2cbe4ca73363b12f3e186083bfa4b0b6
labree <elabre@gmail.com>
2025-04-11 17:27:26 -0300

    added admin parts

--- src/data_manager_app/models.py
+++ src/data_manager_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    disponivel = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.nome
+    
+class Vendedor(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    
