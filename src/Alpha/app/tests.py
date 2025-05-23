85acef07f9010644f327a2c4ac891ad16f421195
Pedro de Castro Kurtz <131386211+nanic1@users.noreply.github.com>
2025-05-23 09:13:40 -0300

    Criando tests.py para realização de requests

--- src/Alpha/app/tests.py
+++ src/Alpha/app/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Administrador
 
+class AdministradorModelTest(TestCase):
+    def test_criacao_administrador(self):
+        admin = Administrador.objects.create(
+            nome="Marcelina Oliveira",
+            idAdministrador=1.01,
+            enderecoEmail="marcelinaoliv@example.com",
+            senha="senha123"
+        )
+        self.assertEqual(admin.nome, "Carlos Silva")
+      
 # Create your tests here.
