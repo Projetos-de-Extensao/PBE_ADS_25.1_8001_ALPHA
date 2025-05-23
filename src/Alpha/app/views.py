83c2d0e68baf3a43265a5ea1cf1bd7dfa6381cb6
Pedro de Castro Kurtz <131386211+nanic1@users.noreply.github.com>
2025-05-23 09:01:36 -0300

    Criação da views.py

--- src/Alpha/app/views.py
+++ src/Alpha/app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Administrador, Domicilio, Morador
+from .serializers import AdministradorSerializer, DomicilioSerializer, MoradorSerializer
+
+class AdministradorViewSet(viewsets.ModelViewSet):
+    queryset = Administrador.objects.all()
+    serializer_class = AdministradorSerializer
+
+class DomicilioViewSet(viewsets.ModelViewSet):
+    queryset = Domicilio.objects.all()
+    serializer_class = DomicilioSerializer
+
+class MoradorViewSet(viewsets.ModelViewSet):
+    queryset = Morador.objects.all()
+    serializer_class = MoradorSerializer
 
 # Create your views here.
