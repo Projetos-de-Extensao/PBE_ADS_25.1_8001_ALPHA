f2ec06566a83ee80fb12c1fcdcf14dd4452c63fe
julia <siodarojuliaa@gmail.com>
2025-05-23 17:49:58 -0300

    Atualizacoes no models e urls.

--- src/Alpha/app/models.py
+++ src/Alpha/app/models.py
@@ -49,12 +49,68 @@ class Domicilio(models.Model):
     distrito = models.CharField(max_length=2)
     especieDomicilio = models.CharField(choices=ESPECIE_DOMICILIO)
     tipoDomicilio = models.CharField(choices=TIPO_DOMICILIO)
-
+    qtdMoradores = models.CharField(max_length=2)
+    qtdCriancas = models.CharField(max_length=2)
     
     def __str__(self):
         return self.nome
 
 class Morador(models.Model):
+    nomeMorador = models.CharField(max_length= 50)
+    sobrenomeMorador = models.CharField(max_length=50)
+    SEXO_MORADOR = [
+        ("MASCULINO", "MASCULINO"),
+        ("FEMININO", "FEMININO")
+    ]
+    dtNascimento = models.IntegerField(2)
+    MES_NASCIMENTO = [
+        ("JANEIRO", "JANEIRO"),
+        ("FEVEREIRO", "FEVEREIRO"),
+        ("MARCO", "MARCO"),
+        ("ABRIL", "ABRIL"),
+        ("MAIO", "MAIO"),
+        ("JUNHO", "JUNHO"),
+        ("JULHO", "JULHO"),
+        ("AGOSTO", "AGOSTO"),
+        ("SETEMBRO", "SETEMBRO"),
+        ("OUTUBRO", "OUTUBRO"),
+        ("NOVEMBRO", "NOVEMBRO"),
+        ("DEZEMBRO", "DEZEMBRO")
+    ]
+    sexoMorador = models.CharField(choices=SEXO_MORADOR)
+    mesNascimento = models.CharField(choices=MES_NASCIMENTO)
+    anoNascimento = models.IntegerField(4)
+    NaoseiMesAno = models.BooleanField()
+    IDADE = [
+        ("UM ANO OU MAIS", "UM ANO OU MAIS"),
+        ("MENOS DE UM ANO", "MENOS DE UM ANO")
+    ]
+    idadeMorador = models.CharField(choices=IDADE)
+    idadeEmAnos = models.IntegerField(4)
+    idadeEmMeses = models.IntegerField(2)
+    RELACAO_CONVIVENCIA = [
+        ("PESSOA RESPONSAVEL PELO DOMICILIO", "PESSOA RESPONSAVEL PELO DOMICILIO"),
+        ("CONJUGE OU COMPANHEIRO(A) DE SEXO DIFERENTE", "CONJUGE OU COMPANHEIRO(A) DE SEXO DIFERENTE"),
+        ("CONJUGE OU COMPANHEIRO(A) DO MESMO SEXO", "CONJUGUE OU COMPANHEIRO(A) DO MESMO SEXO"),
+        ("FILHO(A) DO RESPONSAVEL E DO CONJUGE", "FILHO(A) DO RESPONSAVEL E DO CONJUGE"),
+        ("FILHO(A) SOMENTE DO RESPONSAVEL", "FILHO(A) SOMENTE DO RESPONSAVEL"),
+        ("ENTEADO(A)", "ENTEADO(A)"),
+        ("GENRO OU NORA", "GENRO OU NORA"),
+        ("PAI, MAE, PADRASTO OU MADRASTA", "PAI, MAE, PADRASTO OU MADRASTA"),
+        ("SOGRO(A)", "SOGRO(A)"),
+        ("NETO(A)", "NETO(A)"),
+        ("BISNETO(A)", "BISNETO(A)"),
+        ("IRMAO OU IRMA", "IRMAO OU IRMA"),
+        ("AVO OU AVO", "AVO OU AVO"),
+        ("OUTRO PARENTE", "OUTRO PARENTE"),
+        ("AGREGADO(A)", "AGREGADO(A)"),
+        ("CONVIVENTE", "CONVIVENTE"),
+        ("PENSIONISTA", "PENSIONISTA"),
+        ("EMPREGADO(A) DOMESTICO(A)", "EMPREGADO(A) DOMESTICO(A)"),
+        ("PARENTE DO(A) EMPREGADO(A) DOMESTICO(A)", "PARENTE DO(A) EMPREGADO(A) DOMESTICO(A)"),
+        ("INDIVIDUAL EM DOMICILIO COLETIVO", "INDIVIDUAL EM DOMICILIO COLETIVO")
+    ]
+    relacaoConvivencia = models.CharField(choices=RELACAO_CONVIVENCIA)
 
     def __str__(self):
         return self.nome
