a681817b0a2123079d6027b6baf479f88de2bd25
Pedro de Castro Kurtz <131386211+nanic1@users.noreply.github.com>
2025-05-23 09:12:03 -0300

    Atualizando os modelos do admin.py

--- src/Alpha/app/admin.py
+++ src/Alpha/app/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import Domicilio
+from .models import Administrador, Domicilio, Morador
 
+admin.site.register(Administrador)
 admin.site.register(Domicilio)
-
-
-
-
+admin.site.register(Morador)
