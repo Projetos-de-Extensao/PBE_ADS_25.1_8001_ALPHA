c4b848f3d7675a0387c2850bd627bbce3eae627c
julia <siodarojuliaa@gmail.com>
2025-05-30 17:50:23 -0300

    atualizacao models

--- src/Alpha/app/models.py
+++ src/Alpha/app/models.py
@@ -39,18 +39,75 @@ class Domicilio(models.Model):
     distrito = models.CharField(max_length=50)
     especieDomicilio = models.CharField(max_length=100,choices=ESPECIE_DOMICILIO)
     tipoDomicilio = models.CharField(max_length=100,choices=TIPO_DOMICILIO)
-
-  
+    qtdMoradores = models.CharField(max_length=2, null = True, blank= True)
+    qtdCriancas = models.CharField(max_length=2, null = True, blank= True)
     
     def __str__(self):
         return self.uf
-
+    
 class Morador(models.Model):
     nome = models.CharField(max_length=100, null = True, blank= True)
+    sobrenomeMorador = models.CharField(max_length=50, null = True, blank= True)
+    SEXO_MORADOR = [
+        ("MASCULINO", "MASCULINO"),
+        ("FEMININO", "FEMININO")
+    ]
+    dtNascimento = models.CharField(max_length=2, null = True, blank= True)
+    MES_NASCIMENTO = [
+        ("JANEIRO", "JANEIRO"),
+        ("FEVEREIRO", "FEVEREIRO"),
+        ("MARCO", "MARCO"),
+        ("ABRIL", "ABRIL"),
+        ("MAIO", "MAIO"),
+        ("JUNHO", "JUNHO"),
+        ("JULHO", "JULHO"),
+        ("AGOSTO", "AGOSTO"),
+        ("SETEMBRO", "SETEMBRO"),
+        ("OUTUBRO", "OUTUBRO"),
+        ("NOVEMBRO", "NOVEMBRO"),
+        ("DEZEMBRO", "DEZEMBRO")
+    ]
+    sexoMorador = models.CharField(choices=SEXO_MORADOR, null = True, blank= True)
+    mesNascimento = models.CharField(choices=MES_NASCIMENTO, null = True, blank= True)
+    anoNascimento = models.CharField(max_length=4, null = True, blank= True)
+    NaoseiMesAno = models.BooleanField(null = True, blank= True)
+    IDADE = [
+        ("UM ANO OU MAIS", "UM ANO OU MAIS"),
+        ("MENOS DE UM ANO", "MENOS DE UM ANO")
+    ]
+    idadeMorador = models.CharField(choices=IDADE, null = True, blank= True)
+    idadeEmAnos = models.CharField(max_length=4, null = True, blank= True)
+    idadeEmMeses = models.CharField(max_length=2, null = True, blank= True)
+    RELACAO_CONVIVENCIA = [
+        ("PESSOA RESPONSAVEL PELO DOMICILIO", "PESSOA RESPONSAVEL PELO DOMICILIO"),
+        ("CONJUGE OU COMPANHEIRO(A) DE SEXO DIFERENTE", "CONJUGE OU COMPANHEIRO(A) DE SEXO DIFERENTE"),
+        ("CONJUGE OU COMPANHEIRO(A) DO MESMO SEXO", "CONJUGUE OU COMPANHEIRO(A) DO MESMO SEXO"),
+        ("FILHO(A) DO RESPONSAVEL E DO CONJUGE", "FILHO(A) DO RESPONSAVEL E DO CONJUGE"),
+        ("FILHO(A) SOMENTE DO RESPONSAVEL", "FILHO(A) SOMENTE DO RESPONSAVEL"),
+        ("ENTEADO(A)", "ENTEADO(A)"),
+        ("GENRO OU NORA", "GENRO OU NORA"),
+        ("PAI, MAE, PADRASTO OU MADRASTA", "PAI, MAE, PADRASTO OU MADRASTA"),
+        ("SOGRO(A)", "SOGRO(A)"),
+        ("NETO(A)", "NETO(A)"),
+        ("BISNETO(A)", "BISNETO(A)"),
+        ("IRMAO OU IRMA", "IRMAO OU IRMA"),
+        ("AVO OU AVO", "AVO OU AVO"),
+        ("OUTRO PARENTE", "OUTRO PARENTE"),
+        ("AGREGADO(A)", "AGREGADO(A)"),
+        ("CONVIVENTE", "CONVIVENTE"),
+        ("PENSIONISTA", "PENSIONISTA"),
+        ("EMPREGADO(A) DOMESTICO(A)", "EMPREGADO(A) DOMESTICO(A)"),
+        ("PARENTE DO(A) EMPREGADO(A) DOMESTICO(A)", "PARENTE DO(A) EMPREGADO(A) DOMESTICO(A)"),
+        ("INDIVIDUAL EM DOMICILIO COLETIVO", "INDIVIDUAL EM DOMICILIO COLETIVO")
+    ]
+    relacaoConvivencia = models.CharField(choices=RELACAO_CONVIVENCIA, null = True, blank= True)
+
+   
 
     def __str__(self):
         return self.nome
     
 
+    
 
-# Create your models here.
+
